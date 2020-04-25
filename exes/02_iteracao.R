0b8f126dc887709edf61439eb289a32349f6726c
C. T. Lente <ctlente@users.noreply.github.com>
2020-04-25 15:08:05 +0000

    Parte 1 da aula

--- exes/02_iteracao.R
+++ exes/02_iteracao.R
@@ -1,26 +1,40 @@
 library(httr)
 library(xml2)
 
-# Na página da Wikipédia, encontrar o objeto correspondente à tabela lateral de
-# informações. Pegar apenas os elementos correspondentes a links.
+# Na página da Wikipédia do R, encontrar o objeto correspondente à tabela
+# lateral de informações. Pegar apenas os elementos correspondentes a links.
 
 links <- "https://en.wikipedia.org/wiki/R_language" %>%
   read_html() %>%
   xml_find_all("//table[@class='infobox vevent']//a")
+
+# Exatamente igual
+"https://en.wikipedia.org/wiki/R_language" %>%
+  read_html() %>%
+  xml_find_all("//table[@class='infobox vevent']") %>%
+  xml_find_all(".//a")
 
 head(links)
 
 # Extrair todos os URLs dos links e completá-los com o resto do caminho da
 # Wikipédia. Continuar usando apenas _pipes_.
 
-urls <- "https://en.wikipedia.org/wiki/R_language" %>%
-  read_html() %>%
-  xml_find_all("//table[@class='infobox vevent']//a") %>%
-  xml_attr("href") %>%
-  paste0("https://en.wikipedia.org", .)
+urls <- xml_attr(links, "href")
+urls <- paste0("https://en.wikipedia.org", urls)
 
 head(urls)
 
 # Baixar todas as páginas da Wikipédia. Dicas: use `possibly()` para impedir erros
 # quando o URL for inválido; procure saber sobre a função `map2()` para iterar em
 # mais de uma lista; salve os arquivos com `GET(..., write_disk(path))`.
+
+# 1
+map(urls, possibly(GET, ...))
+
+# 2
+caminhos <- c("1.html", "2.html", ...) # paste0(1:10, ".html")
+
+f <- function(url, caminho) {
+  GET(url, write_disk(caminho))
+}
+map2(urls, caminhos_para_salvar, f)
---
+++ exes/06_extras.R
@@ -0,0 +1,45 @@
+library(xml2)
+library(httr)
+library(purrr)
+
+# Funções principais
+html <- read_html("docs/static/html_exemplo.html")
+nodes <- xml_find_all(html, "//p")
+xml_text(nodes)
+xml_attrs(nodes)
+
+# xml_attrs -> todos os atributos
+# xml_attr  -> um atributo com nome
+
+# Diferença entre // e /
+xml_find_all(html, "/html/body/p")
+xml_find_all(html, "//p")
+
+# Parentesco
+xml_parent(nodes)
+xml_parents(nodes)
+
+# * ou tag
+html <- read_html("docs/static/html_exemplo_css_a_parte.html")
+xml_find_all(html, "//*[@class='azul']")
+xml_find_all(html, "//p[@class='azul']")
+
+
+# Possibly com map
+maybe_read_html <- possibly(read_html, NULL)
+
+read_html("https://errado.que")
+maybe_read_html("https://errado.que")
+
+urls <- c(
+  "https://en.wikipedia.org/wiki/R_language",
+  "https://en.wikipedia.org/wiki/Python_(programming_language)",
+  "https://errado.que"
+)
+
+map(urls, maybe_read_html)
+
+
+
+
+
